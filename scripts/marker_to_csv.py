b52a698b4e3c4bdd1700d842fdb237f2e7cd4206
Ryan Constantino <ryan.constantino93@gmail.com>
2017-02-13 01:48:00 -0800

    Begin implementing Note and delete confirmation

--- scripts/marker_to_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def main():
-    with open('marker.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, dialect='excel')
-        writer.writerow(['ryan', 'constantino'])
-        writer.writerow(['joseph', 'tan'])
-
-main()
